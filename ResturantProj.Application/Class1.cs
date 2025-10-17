990823b06d907c6cf42d9987609388f8b7da07bd
Mohmmed Sharqawy <msharqawy21@gmail.com>
2025-10-17 18:25:53 +0200

    onionImplementation

---
+++ ResturantProj.Application/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Intrinsics.X86;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ResturantProj.Application
+{
+    //    DTOs / ViewModels
+
+    //Service interfaces(e.g., ICategoryService, IMenuItemService)
+
+    //Use cases / application logic
+}
---
+++ ResturantProj.Infrastructure/Class1.cs
@@ -0,0 +1,4 @@
+﻿namespace ResturantProj.Infrastructure
+{
+    
+}
